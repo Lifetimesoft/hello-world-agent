cc47de312b66403b566fe6946ffbb70660f3a0f1
DOE-PC-SERVER\Doesystem <doe.system@gmail.com>
2026-04-21 22:31:17 +0700

    update agent-sdk version

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import { defineAgent } from "@lifetimesoft/agent-sdk"
 
 export default defineAgent({
-  async run(ctx) {
-    while (true) {
-      ctx.log.info("hello world")
-      await new Promise(resolve => setTimeout(resolve, 10_000))
-    }
-  },
+    async run(ctx) {
+        while (true) {
+            ctx.log.info('scheduler type: ' + ctx.config.scheduler?.type)
+
+            ctx.log.info("hello world")
+        }
+    },
 })
