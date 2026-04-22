2f24f8875b0a5185a7579734ad50b1fbcd2a020d
DOE-PC-SERVER\Doesystem <doe.system@gmail.com>
2026-04-23 05:16:04 +0700

    chore: update agent.json and package.json and remove while true

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,7 @@ import { defineAgent } from "@lifetimesoft/agent-sdk"
 
 export default defineAgent({
     async run(ctx) {
-        while (true) {
-            ctx.log.info('scheduler type: ' + ctx.config.scheduler?.type)
-
-            ctx.log.info("hello world")
-        }
+        ctx.log.info('scheduler type: ' + ctx.config.scheduler?.type)
+        ctx.log.info("hello world")
     },
 })
